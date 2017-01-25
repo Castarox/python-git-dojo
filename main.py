1acd03d3b3ec6898228512c50a4b24322d3ac190
castarox <mika.kamil12@gmail.com>
2017-01-25 09:42:29 +0100

    create main function to test

---
+++ main.py
@@ -0,0 +1,15 @@
+from gittie_helper import *
+
+def main():
+    house = GittieHelper()
+    house.set_temperature(20)
+    house.set_humidity(0.80)
+    house.set_air_pollution(8)
+    if house.is_home_save():
+        print('yey')
+    else:
+        print('nope')
+
+
+if __name__ == '__main__':
+    main()
