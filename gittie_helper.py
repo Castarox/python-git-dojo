c242bd8d030dec0dc7efd19a77674eebe032bb33
Michał Osak <osakmichal@gmail.com>
2017-01-25 09:27:46 +0100

    B tasks

--- gittie_helper.py
+++ gittie_helper.py
@@ -4,8 +4,11 @@ class GittieHelper():
         """
         Initialize attributes with default value
         """
-        adsf
-        pass
+        self.air_pollution_level = 10
+        self.day_number = 10
+
+
+
 
     def set_temperature(self, temperature_degree):
         """
@@ -17,7 +20,7 @@ class GittieHelper():
     def set_humidity(self, humidity_value):
         """
         Method sets humidity level to attribute and validate input
-        :param humidity_value:
+        :param humidity_value:pass
         """
         pass
 
@@ -26,17 +29,28 @@ class GittieHelper():
         Method sets air pollution level to attribute and validate input
         :param air_pollution_level:
         """
-        pass
+        try:
+            self.air_pollution_level = int(air_pollution_level)
+        except ValueError:
+            print('ValueError')
+
 
     def set_day_of_the_year(self, day_number):
         """
         Method sets day number from beginning of the year to attribute and validate input
         :param day_number:
         """
-        pass
+        try:
+            self.day_number = int(day_number)
+        except ValueError:
+            print('ValueError')
 
     def get_value(self):
         """
         Method should calculate if exiting home is safe for gittie
         """
-        pass
+
+        if self.air_pollution_level < 10 and self.humidity_value > 0.5 and temperature_degree > 15:
+            return true
+        else:
+            return false
